af5fe1dc7ebc739010e803743e798a9472510b7c
FeraTaTa <fhathaf@gmail.com>
2019-10-09 23:12:17 +0100

    bug fix - it stil worked because the object were passed in unity inspector.

--- 1 Moving/Waypoints/Assets/WaypointFollow.cs
+++ 1 Moving/Waypoints/Assets/WaypointFollow.cs
@@ -15,7 +15,7 @@ public class WaypointFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        waypoints = GameObject.FindGameObjectsWithTag("waypoints");
+        waypoints = GameObject.FindGameObjectsWithTag("waypoint");
     }
 
     // Update is called once per frame
