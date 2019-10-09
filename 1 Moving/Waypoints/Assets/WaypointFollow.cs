b7eca169b37491faf26b40ecfaf420d5ca8bc55c
FeraTaTa <fhathaf@gmail.com>
2019-10-09 23:45:27 +0100

    Using Unity standard assets: Waypoint circuit

--- 1 Moving/Waypoints/Assets/WaypointFollow.cs
+++ 1 Moving/Waypoints/Assets/WaypointFollow.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class WaypointFollow : MonoBehaviour
 {
-    public GameObject[] waypoints;
+    //public GameObject[] waypoints;
+    public UnityStandardAssets.Utility.WaypointCircuit circuit;
     int currentWP = 0;
 
     float speed = 5.0f;
@@ -15,17 +16,17 @@ public class WaypointFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+        //waypoints = GameObject.FindGameObjectsWithTag("waypoint");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (waypoints.Length == 0) return;
+        if (circuit.Waypoints.Length == 0) return;
 
-        Vector3 goal = new Vector3(waypoints[currentWP].transform.position.x,
+        Vector3 goal = new Vector3(circuit.Waypoints[currentWP].position.x,
                                             this.transform.position.y,
-                                            waypoints[currentWP].transform.position.z);
+                                            circuit.Waypoints[currentWP].position.z);
         Vector3 direction = goal - this.transform.position;
 
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
@@ -35,7 +36,7 @@ public class WaypointFollow : MonoBehaviour
         if (direction.magnitude < accuracy)
         {
             currentWP++;
-            if (currentWP >= waypoints.Length)
+            if (currentWP >= circuit.Waypoints.Length)
             {
                 currentWP = 0;
             }
