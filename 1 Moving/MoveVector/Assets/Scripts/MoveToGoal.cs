615d4177585e7b314337271e31d0400a109a407f
FeraTaTa <fhathaf@gmail.com>
2019-10-09 20:41:13 +0100

    Move to a goal and stop, translate in world space instead of local, LookAt()

---
+++ 1 Moving/MoveVector/Assets/Scripts/MoveToGoal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float accuracy = 0.01f;
+    public Transform goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(goal.position);
+        Vector3 direction = goal.position - this.transform.position;
+        Debug.DrawRay(this.transform.position, direction, Color.green);
+        if (direction.magnitude > accuracy)
+        {
+            this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        }
+        
+    }
+}
