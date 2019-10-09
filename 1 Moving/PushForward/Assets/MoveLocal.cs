28733701cdddc6ad0d6396ceae81a2362256d3dd
FeraTaTa <fhathaf@gmail.com>
2019-10-09 21:24:24 +0100

    Slerping

--- 1 Moving/PushForward/Assets/MoveLocal.cs
+++ 1 Moving/PushForward/Assets/MoveLocal.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class MoveLocal : MonoBehaviour
 {
     public Transform goal;
-    float speed = 0.5f;
     float accuracy = 0.1f;
+
+    float speed = 0.5f;
+    float rotSpeed = 0.4f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +20,13 @@ public class MoveLocal : MonoBehaviour
     void LateUpdate()
     {
         //create a goal that ignores vertical distance
-        Vector3 LookAtgoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
-        
-        this.transform.LookAt(LookAtgoal);
+        Vector3 LookAtgoal = new Vector3(goal.position.x, 
+                                        this.transform.position.y, goal.position.z);
+        Vector3 direction = LookAtgoal - this.transform.position;
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                                Quaternion.LookRotation(direction), 
+                                                Time.deltaTime * rotSpeed);
         //Translate between this and the goal in the x-z plane, stopping when within acciracy
         if (Vector3.Distance(transform.position, LookAtgoal) > accuracy){
             this.transform.Translate(0, 0, speed * Time.deltaTime);
