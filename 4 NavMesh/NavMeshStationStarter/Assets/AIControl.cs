448681fa4376033c142324eb5071341eba7e86e5
FeraTaTa <fhathaf@gmail.com>
2019-10-10 21:52:49 +0100

    NavMesh functional with multiple agents on a baked surface

---
+++ 4 NavMesh/NavMeshStationStarter/Assets/AIControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIControl : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+}
