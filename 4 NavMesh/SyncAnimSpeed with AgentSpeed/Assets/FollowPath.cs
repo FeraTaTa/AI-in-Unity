a0ea209d4fd58be5e90d8e18a7465c1e8f08fb0c
FeraTaTa <fhathaf@gmail.com>
2019-10-15 18:43:11 +0100

    Driven by NavMeshAgent, Animator value "vel" added which is read from the agent, speed reduced to 0.25, heigh mesh added to stop agent floating above ground, but now steps are choppy.

--- 4 NavMesh/SyncAnimSpeed with AgentSpeed/Assets/FollowPath.cs
+++ 4 NavMesh/SyncAnimSpeed with AgentSpeed/Assets/FollowPath.cs
@@ -8,12 +8,14 @@ public class FollowPath : MonoBehaviour
     NavMeshAgent agent;
     public GameObject[] waypoints;
     int currentWP = 0;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
         agent.SetDestination(waypoints[currentWP].transform.position);
+        anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,5 +27,6 @@ public class FollowPath : MonoBehaviour
             if (currentWP >= waypoints.Length) currentWP = 0;
             agent.SetDestination(waypoints[currentWP].transform.position);
         }
+        anim.SetFloat("vel", agent.velocity.magnitude);
     }
 }
