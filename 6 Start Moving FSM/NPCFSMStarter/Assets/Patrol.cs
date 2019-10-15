e43d1f2dd2ad3eef2ecb62a9e3c3d45b44942540
FeraTaTa <fhathaf@gmail.com>
2019-10-15 21:05:36 +0100

    Created an FSM using a script attached to the unity animator controller object

---
+++ 6 Start Moving FSM/NPCFSMStarter/Assets/Patrol.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : StateMachineBehaviour
+{
+    GameObject NPC;
+    GameObject[] waypoints;
+    int currentWP;
+
+    private void Awake()
+    {
+        waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+    }
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        NPC = animator.gameObject;
+        currentWP = 0;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (waypoints.Length == 0) return;
+        if(Vector3.Distance(waypoints[currentWP].transform.position, NPC.transform.position) < 3f)
+        {
+            currentWP++;
+            if(currentWP >= waypoints.Length)
+            {
+                currentWP = 0;
+            }
+        }
+
+        //rotate towards target
+        var direction = waypoints[currentWP].transform.position - NPC.transform.position;
+        NPC.transform.rotation = Quaternion.Slerp(NPC.transform.rotation, 
+                                                    Quaternion.LookRotation(direction), 
+                                                    1f * Time.deltaTime);
+        NPC.transform.Translate(0, 0, 2f * Time.deltaTime);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
